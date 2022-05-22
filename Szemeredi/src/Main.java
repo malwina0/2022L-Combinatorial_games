7cf192fa6434cab74b708354a153ae0a9ab22d1a
alexmal01 <alexander.mal2100@gmail.com>
2022-05-22 02:12:58 +0200

    remove colors for Windows version

--- Szemeredi/src/Main.java
+++ Szemeredi/src/Main.java
@@ -2,25 +2,21 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
     public static void main(String[] args) throws Exception {
 
         Game game = Game.DataInput();
-        int gameIters = 1;
+        int gameIters = 0;
         while (true){
-            System.out.println(ANSI_BLUE_BACKGROUND + "-----------------------" + "Tura nr " + gameIters + "-----------------------" + ANSI_RESET);
+            System.out.println("---------------------" + "Tura nr " + gameIters + "-----------------------");
             System.out.println("Zbiór do gry:");
             System.out.println(game.Set);
             int len1;
-            if (gameIters == 1) len1 = game.computerFirstMove(); else len1 = game.ComputerMove();
+            if (gameIters == 0) len1 = game.computerFirstMove(); else len1 = game.ComputerMove();
             if (len1 >= game.k) {
                 System.out.println("KOMPUTER WYGRAŁ!");
                 Map<Integer, List<Integer>> winning = ProgressionChecker.CheckProgressions(game.ComputerSet);
                 int key = winning.keySet().iterator().next();
-                System.out.println("Wygrywający ciąg: " + ANSI_RED + winning.get(key) + ANSI_RESET);
+                System.out.println("Wygrywający ciąg: " + winning.get(key));
                 TimeUnit.SECONDS.sleep(5);
                 break;
             }
@@ -37,7 +33,7 @@ public class Main {
                 System.out.println("GRACZ WYGRAŁ!");
                 Map<Integer, List<Integer>> winning = ProgressionChecker.CheckProgressions(game.PlayerSet);
                 int key = winning.keySet().iterator().next();
-                System.out.println("Wygrywający ciąg: " + ANSI_GREEN + winning.get(key) + ANSI_RESET);
+                System.out.println("Wygrywający ciąg: " + winning.get(key));
                 TimeUnit.SECONDS.sleep(5);
                 break;
             }
